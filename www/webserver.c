c0a178c88ca0e8e8807c2620dd9c2bfd98df2da7
user <aast7950@colorado.edu>
2019-09-29 15:49:22 -0600

    First commit

---
+++ www/webserver.c
@@ -0,0 +1,192 @@
+/* 
+ *Aaron Steiner PA2
+ *
+ * tcpechosrv.c - A concurrent TCP echo server using threads
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>      /* for fgets */
+#include <strings.h>     /* for bzero, bcopy */
+#include <unistd.h>      /* for read, write */
+#include <sys/socket.h>  /* for socket use */
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <fcntl.h>
+
+#define MAXLINE  8192  /* max text line length */
+#define MAXBUF   8192  /* max I/O buffer size */
+#define LISTENQ  1024  /* second argument to listen() */
+
+int open_listenfd(int port);
+//void echo(int connfd);
+void *thread(void *vargp);
+
+
+/*read the file and return its contents*/
+int rFile(int file, char* sBuf, int s,int* numBytes)
+{
+    
+    *numBytes = read(file,sBuf,s);
+    
+    if(*numBytes < MAXLINE){
+        return 1;
+    }
+    
+    return 0;
+    
+} 
+
+/*get the file size*/
+long int fileSize(char filename[]) 
+{ 
+    //printf("In fileSize function\n");
+    FILE* file = fopen(filename, "r"); 
+  
+    if (file == NULL) { 
+        printf("File Not Found!\n"); 
+        return -1; 
+    } 
+  
+    fseek(file, 0L, SEEK_END); 
+   
+    long int number = ftell(file); 
+   
+    fclose(file); 
+    //printf("Returning: %ld\n",number);
+    return number; 
+}
+
+int get(int connfd){
+    size_t n; 
+    char buf[MAXLINE];
+    char rString[MAXLINE];
+    char Default[14];
+    char fstring[MAXLINE];
+    int numBytes = 0;
+    //char httpmsg[]="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:32\r\n\r\n<html><h1>Hello CSCI4273 Course!</h1>"; 
+    char* httpmsg;
+    n = read(connfd, buf, MAXLINE);
+    printf("server received the following request:\n%s\n",buf);
+    
+    //strcpy(temp,buf);
+    for(int i=0; i < 14; i++){
+        Default[i] = buf[i];
+    }
+    //printf("Buff at 14 %c\n",buf[14]);
+    //printf("Size of temp %ld\n",sizeof(temp));
+    //printf("Temp is: %s\n",temp);
+    //printf("String Comparison: %d\n", strcmp(temp, "GET / HTTP/1.1"));
+    if(strcmp(Default, "GET / HTTP/1.1") == 0){
+        //printf("Requesting Default webpage\n");
+        
+        long int fsize = fileSize("index.html");
+        
+        int file;
+        file = open("index.html", O_RDONLY); 
+        rFile(file,fstring,MAXLINE,&numBytes); //= "\r\n\r\n<html><h1>Default</h1>";
+
+        httpmsg="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:";
+
+        char sizeString[4];
+        sprintf(sizeString, "%ld", fsize);
+        //printf("sizeString %s\n",sizeString);
+        strcat(rString, httpmsg);
+        //printf("rString%s\n",rString);
+        strcat(rString,sizeString);
+        //printf("httpmsg after first cat%s\n",rString);
+        strcat(rString,fstring);
+        //printf("httpmsg after second%s\n",rString);
+    }
+    strcpy(buf,rString);
+    printf("server returning a http message with the following content.\n%s\n",buf);
+    write(connfd, buf,strlen(rString));
+}
+
+
+int main(int argc, char **argv) 
+{
+    int listenfd, *connfdp, port, clientlen=sizeof(struct sockaddr_in);
+    struct sockaddr_in clientaddr;
+    pthread_t tid; 
+
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(0);
+    }
+    port = atoi(argv[1]);
+
+    listenfd = open_listenfd(port);
+    while (1) {
+	   connfdp = malloc(sizeof(int));
+	   *connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
+	   pthread_create(&tid, NULL, thread, connfdp);
+    }
+}
+
+/* thread routine */
+void * thread(void * vargp) 
+{  
+    int connfd = *((int *)vargp);
+    get(connfd);
+
+    pthread_detach(pthread_self()); 
+    free(vargp);
+    //echo(connfd);
+    close(connfd);
+    return NULL;
+}
+
+/*
+ * echo - read and echo text lines until client closes connection
+ */
+// void echo(int connfd) 
+// {
+//     size_t n; 
+//     char buf[MAXLINE]; 
+//     char httpmsg[]="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:32\r\n\r\n<html><h1>Hello CSCI4273 Course!</h1>"; 
+
+//     n = read(connfd, buf, MAXLINE);
+//     printf("server received the following request:\n%s\n",buf);
+//     strcpy(buf,httpmsg);
+//     printf("server returning a http message with the following content.\n%s\n",buf);
+//     write(connfd, buf,strlen(httpmsg));
+    
+// }
+
+/* 
+ * open_listenfd - open and return a listening socket on port
+ * Returns -1 in case of failure 
+ */
+int open_listenfd(int port) 
+{
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+  
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        return -1;
+
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+                   (const void *)&optval , sizeof(int)) < 0)
+        return -1;
+
+    /* listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
+        return -1;
+    return listenfd;
+} /* end open_listenfd */
+
+
